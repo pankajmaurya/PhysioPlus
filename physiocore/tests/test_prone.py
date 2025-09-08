5f89e8981ae2dc284766fe7f947e360413f8456d
Pankaj Maurya <pankajmaurya@gmail.com>
2025-09-08 19:12:38 +0530

    add a test which is failing right now

--- physiocore/tests/test_prone.py
+++ physiocore/tests/test_prone.py
@@ -3,6 +3,23 @@ import os
 from physiocore.any_prone_straight_leg_raise import AnyProneSLRTracker
 
 class TestAnyProneSLRTracker(unittest.TestCase):
+    def test_any_prone_long_hold_video(self):
+        tracker = AnyProneSLRTracker()
+        
+        # Override HOLD_SECS
+        # tracker.hold_secs = 1.0
+        
+        # Get the path to the video file
+        video_path = os.path.join(os.path.dirname(__file__), 'prone-long-hold.mp4')
+        
+        # Process the video without displaying GUI
+        count = tracker.process_video(video_path=video_path, display=True)
+        # In development mode, try running with display ON too.
+        # count = tracker.process_video(video_path=video_path, display=True)
+        
+        # Assert the count is 1
+        self.assertEqual(count, 1)
+
 
     def test_any_prone_video(self):
         tracker = AnyProneSLRTracker()
@@ -14,7 +31,7 @@ class TestAnyProneSLRTracker(unittest.TestCase):
         video_path = os.path.join(os.path.dirname(__file__), 'prone-mini-test.mp4')
         
         # Process the video without displaying GUI
-        count = tracker.process_video(video_path=video_path, display=False)
+        count = tracker.process_video(video_path=video_path, display=True)
         # In development mode, try running with display ON too.
         # count = tracker.process_video(video_path=video_path, display=True)
         
